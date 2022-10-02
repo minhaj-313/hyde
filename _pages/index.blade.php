f6e21576f6ce9e40544a55274b517035085903c1
Shaikh Minhaj <shaikhumarms999@gmail.com>
2022-10-02 20:21:30 +0530

    Update index.blade.php

--- _pages/index.blade.php
+++ _pages/index.blade.php
@@ -26,6 +26,19 @@
             padding-top: .5rem;
             padding-bottom: 1rem;
         }
+        /* scroll bar */
+        *::-webkit-scrollbar,
+        *::-webkit-scrollbar-thumb {
+        width: 26px;
+        border-radius: 13px;
+        background-clip: padding-box;
+        border: 10px solid transparent;
+        }
+
+        *::-webkit-scrollbar-thumb {        
+        box-shadow: inset 0 0 0 10px;
+        }
+
     </style>
 </head>
 
