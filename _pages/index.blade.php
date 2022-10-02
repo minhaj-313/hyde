4f8d946112596268d1762cf57b73a618ce968eec
Shaikh Minhaj <shaikhumarms999@gmail.com>
2022-10-02 20:34:57 +0530

    Add Scroll Bar

--- _pages/index.blade.php
+++ _pages/index.blade.php
@@ -26,19 +26,7 @@
             padding-top: .5rem;
             padding-bottom: 1rem;
         }
-        /* scroll bar */
-        *::-webkit-scrollbar,
-        *::-webkit-scrollbar-thumb {
-        width: 26px;
-        border-radius: 13px;
-        background-clip: padding-box;
-        border: 10px solid transparent;
-        }
-
-        *::-webkit-scrollbar-thumb {        
-        box-shadow: inset 0 0 0 10px;
-        }
-
+       
     </style>
 </head>
 
